deee1c8335bdf2a56f1b142038c7e04a5f464fee
Nereuxofficial <37740907+Nereuxofficial@users.noreply.github.com>
2024-06-11 13:45:11 +0200

    feat: Added time printing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use dotenvy::dotenv;
+use gcloud_sdk::google_rest_apis::compute_v1::configuration::Configuration;
 use gcloud_sdk::google_rest_apis::compute_v1::instances_api::{
-    compute_instances_insert, ComputePeriodInstancesPeriodInsertParams,
+    compute_instances_get, compute_instances_insert, compute_instances_set_metadata,
+    ComputePeriodInstancesPeriodGetParams, ComputePeriodInstancesPeriodInsertParams,
 };
 use gcloud_sdk::google_rest_apis::compute_v1::machine_images_api::{
     compute_machine_images_list, ComputePeriodMachineImagesPeriodListParams,
@@ -8,8 +10,14 @@ use gcloud_sdk::google_rest_apis::compute_v1::machine_images_api::{
 use gcloud_sdk::google_rest_apis::compute_v1::scheduling::{
     InstanceTerminationAction, OnHostMaintenance, ProvisioningModel,
 };
-use gcloud_sdk::google_rest_apis::compute_v1::{Instance, Scheduling};
+use gcloud_sdk::google_rest_apis::compute_v1::{Instance, MetadataItemsInner, Scheduling};
+use gcloud_sdk::GoogleRestApi;
 use rand::{thread_rng, Rng};
+use std::fmt::Debug;
+use std::fs::read_to_string;
+use std::net::{IpAddr, Ipv4Addr};
+use std::str::FromStr;
+use std::time::Instant;
 use tracing::info;
 use virt::connect::Connect;
 use virt::domain::Domain;
@@ -20,12 +28,19 @@ async fn main() {
     dotenv().unwrap();
     tracing_subscriber::fmt::init();
     info!("Migration starting... Requesting new machine to be started...");
-    create_instance_with_image().await;
+    let start = Instant::now();
+    let ip_address = create_instance_with_image().await;
     migrate(
         Some("qemu:///session".into()),
-        Some("ssh+qemu://192.168.0.1/system".into()),
+        Some(format!("ssh+qemu://{}/session", ip_address)),
         "example-vm",
-    )
+    );
+    let duration = start.elapsed();
+    info!(
+        "Migration completed in {:?}. Time left: {} seconds",
+        duration,
+        30 - duration.as_secs()
+    );
 }
 
 async fn get_zone() -> Result<String, reqwest::Error> {
@@ -44,7 +59,7 @@ async fn get_zone() -> Result<String, reqwest::Error> {
         .to_string())
 }
 
-async fn create_instance_with_image() {
+async fn create_instance_with_image() -> IpAddr {
     let mut rng = thread_rng();
     let zone = get_zone()
         .await
@@ -53,12 +68,12 @@ async fn create_instance_with_image() {
         .await
         .unwrap_or_else(|| std::env::var("GCP_PROJECT").unwrap());
     info!(
-        "Creating instance in project '{}' and zone '{}'",
+        "Creating operation in project '{}' and zone '{}'",
         project, zone
     );
     let client = gcloud_sdk::GoogleRestApi::new().await.unwrap();
     let compute_v1_config = client.create_google_compute_v1_config().await.unwrap();
-    let machine_images = compute_machine_images_list(
+    let mut machine_images = compute_machine_images_list(
         &compute_v1_config,
         ComputePeriodMachineImagesPeriodListParams {
             project: project.clone(),
@@ -69,20 +84,41 @@ async fn create_instance_with_image() {
     .unwrap()
     .items
     .unwrap();
-    let machine_image = machine_images.first().unwrap();
+    let mut machine_image = machine_images.first_mut().unwrap();
     info!("Machine image: {:?}", machine_image.name.clone());
-    let instance = compute_instances_insert(
+    // Edit the metadata to add our machine's ssh public key while preserving the previous ssh keys
+    let mut metadata_items = machine_image
+        .instance_properties
+        .as_mut()
+        .unwrap()
+        .metadata
+        .as_mut()
+        .unwrap()
+        .items
+        .as_mut()
+        .unwrap();
+    metadata_items.into_iter().for_each(|item| {
+        if item.key.as_ref().unwrap() == "ssh-keys" {
+            let mut value = item.value.as_ref().unwrap().clone();
+            value.push_str("\n");
+            value.push_str(get_ssh_key().as_str());
+            item.value = Some(value);
+        }
+    });
+
+    let name = format!("nested-qemu-{}", rng.gen::<u32>());
+    let operation = compute_instances_insert(
         &compute_v1_config,
         ComputePeriodInstancesPeriodInsertParams {
             project: project.clone(),
-            zone,
+            zone: zone.clone(),
             source_machine_image: Some(format!(
                 "projects/{}/global/machineImages/{}",
                 project,
                 machine_image.name.as_ref().unwrap()
             )),
             instance: Some(Instance {
-                name: Some(format!("nested-qemu-{}", rng.gen::<u32>())),
+                name: Some(name.clone()),
                 scheduling: Some(Box::new(Scheduling {
                     preemptible: Some(false),
                     provisioning_model: Some(ProvisioningModel::Standard),
@@ -96,7 +132,59 @@ async fn create_instance_with_image() {
     )
     .await
     .unwrap();
-    info!("Instance created: {:?}", instance);
+    info!("Instance created: {:?}", operation);
+    // Maybe we could also give back the instance
+    let details = loop {
+        let res = compute_instances_get(
+            &compute_v1_config,
+            ComputePeriodInstancesPeriodGetParams {
+                project: project.clone(),
+                zone: zone.clone(),
+                instance: name.clone(),
+                ..Default::default()
+            },
+        )
+        .await;
+        if let Ok(details) = res {
+            match details.network_interfaces.clone() {
+                Some(network_interfaces) => {
+                    // We can safely unwrap here since network_interfaces would be None if the does not yet have any
+                    if network_interfaces.first().unwrap().network_ip.is_none() {
+                        continue;
+                    }
+                    break details;
+                }
+                _ => continue,
+            }
+        } else {
+            continue;
+        }
+    };
+    let internal_ip_address = details
+        .network_interfaces
+        .unwrap()
+        .first()
+        .unwrap()
+        .network_ip
+        .clone()
+        .unwrap();
+    info!("Instance internal ip: {}", internal_ip_address);
+    // Simultaneously we should also probably add our ssh key to the target machine to allow for migration
+
+    return IpAddr::V4(
+        Ipv4Addr::from_str(&internal_ip_address)
+            .unwrap_or_else(|_| panic!("Failed to parse ip address: {}", internal_ip_address)),
+    );
+}
+
+fn get_ssh_key() -> String {
+    let home_path =
+        std::env::var("HOME").expect("HOME not found in environment. Please provide a home path");
+    let paths = ["/.ssh/id_rsa.pub", "/.ssh/id_ed25519.pub"];
+    paths
+        .iter()
+        .find_map(|path| read_to_string(format!("{home_path}/{path}")).ok())
+        .unwrap_or_else(|| panic!("Failed to read ssh key"))
 }
 
 fn migrate(src_uri: Option<String>, dst_uri: Option<String>, dname: &str) {
